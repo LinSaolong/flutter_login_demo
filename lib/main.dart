0b6645ae28ab09e492f16ef96dea21ebd3e8e9ae
Saolong Lin <60967691+LinSaolong@users.noreply.github.com>
2023-09-28 15:56:12 +0700

    new

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("GG");
+    print("GG");
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
